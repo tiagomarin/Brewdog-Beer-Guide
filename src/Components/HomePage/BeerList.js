8839f84838de0cd6791ee0b99d6bfb6c1e03e337
Tiago Marin <tiago.lelinski@gmail.com>
2022-09-13 12:16:27 -0300

    add filter to home page

--- src/Components/HomePage/BeerList.js
+++ src/Components/HomePage/BeerList.js
@@ -1,42 +1,108 @@
 import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch, shallowEqual } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Beer from './Beer';
-import { getBeers } from '../../Redux/beersSlice';
+import {
+  getBeers,
+  changeFilter,
+  veryLow,
+  low,
+  medium,
+  high,
+  veryHigh,
+  ultraHigh,
+} from '../../Redux/beersSlice';
 
 const BeerList = () => {
   const dispatch = useDispatch();
-  const beerList = useSelector((state) => state.beers);
+
+  const filter = useSelector((state) => state.beers.filter);
+  const beerList = useSelector((state) => state.beers.allBeers);
+  const filteredBeerList = useSelector((state) => state.beers.filtered || [], shallowEqual);
+
   useEffect(() => {
     if (!beerList.length) {
       dispatch(getBeers());
     }
   });
 
+  const bitternessFilterHandler = (filter) => {
+    switch (filter) {
+      case 'Very low':
+        dispatch(veryLow());
+        break;
+      case 'Low':
+        dispatch(low());
+        break;
+      case 'Medium':
+        dispatch(medium());
+        break;
+      case 'High':
+        dispatch(high());
+        break;
+      case 'Very high':
+        dispatch(veryHigh());
+        break;
+      case 'Ultra high':
+        dispatch(ultraHigh());
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
     <ul className="beer-list-page">
-      <h2>list of beers of a selected IBU</h2>
-      {beerList.map((item) => (
-        <Link
-          to="/BeerDetails"
-          state={item}
-          key={item.id}
-        >
-          <Beer
-            beerData={item}
-          />
-        </Link>
-      ))}
+      <h2>list of ALL beers</h2>
+      {/* filter dropdown */}
+      <select
+        className="filter-menu"
+        name="filter"
+        onChange={(e) => {
+          dispatch(changeFilter(e.target.value));
+          bitternessFilterHandler(filter);
+        }}
+      >
+        <option value="">All Beers</option>
+        <option value="Very low">very low</option>
+        <option value="Low">Low</option>
+        <option value="Medium">Medium</option>
+        <option value="High">High</option>
+        <option value="Very high">Very high</option>
+        <option value="Ultra high">Ultra high</option>
+      </select>
+      {/* <button
+        type="button"
+        onClick={bitternessFilterHandler(filter)}
+      >
+        filter
+      </button> */}
+      {/* list items */}
+      {filter === '' || filter === 'All beers'
+        ? beerList.map((item) => (
+          <Link
+            to="/BeerDetails"
+            state={item}
+            key={item.id}
+          >
+            <Beer
+              beerData={item}
+            />
+          </Link>
+        ))
+        : filteredBeerList.map((item) => (
+          <Link
+            to="/BeerDetails"
+            state={item}
+            key={item.id}
+          >
+            <Beer
+              beerData={item}
+            />
+          </Link>
+        ))}
     </ul>
   );
 };
 
 export default BeerList;
-
-/* <Link
-state={hero}
-className="characters-link"
-key={`heroid-${hero.id}`}
-to="Comics">
-<Character newhero={hero} />
-</Link> */
---
+++ src/Components/HomePage/BitternessFilter.js
@@ -0,0 +1,25 @@
+// import React from 'react';
+
+// const BitternessFilter = (props) => {
+//   const {setBitterness} = props
+//   return (
+//     <select
+//       className="filter-menu"
+//       name="filter"
+//       onChange={(e) => {
+//         setBitterness(e.target.value);
+//       }}
+//       required
+//     >
+//       <option value="">filter by bitterness</option>
+//       <option value="Very low">very low</option>
+//       <option value="Low">Low</option>
+//       <option value="Medium">Medium</option>
+//       <option value="High">High</option>
+//       <option value="Very high">Very high</option>
+//       <option value="Ultra high">Ultra high</option>
+//     </select>
+//   )
+// }
+
+// export default BitternessFilter;
